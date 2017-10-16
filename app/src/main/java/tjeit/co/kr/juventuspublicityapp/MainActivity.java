bf0c78e7a6c986aaa30d6c753fac6a11d2480f3b
henylee <asqaws1@gmail.com>
2017-10-16 11:47:26 +0900

    액션바 커스터마이징 반 성공!

--- app/src/main/java/tjeit/co/kr/juventuspublicityapp/MainActivity.java
+++ app/src/main/java/tjeit/co/kr/juventuspublicityapp/MainActivity.java
@@ -1,13 +1,44 @@
 package tjeit.co.kr.juventuspublicityapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setCustomActionBar();
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    private void setCustomActionBar() {
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        Toolbar parent = (Toolbar) getSupportActionBar().getCustomView().getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+        getSupportActionBar().setElevation(0);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
     }
 }
