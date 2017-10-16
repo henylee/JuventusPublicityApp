1111ab2f92958906bf4d9ff6568e1c22614ecb08
henylee <asqaws1@gmail.com>
2017-10-16 11:33:40 +0900

    시작

---
+++ app/src/main/java/tjeit/co/kr/juventuspublicityapp/BaseActivity.java
@@ -0,0 +1,18 @@
+package tjeit.co.kr.juventuspublicityapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by joeun on 2017-10-16.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+
+}
---
+++ app/src/main/java/tjeit/co/kr/juventuspublicityapp/MainActivity.java
@@ -0,0 +1,13 @@
+package tjeit.co.kr.juventuspublicityapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
