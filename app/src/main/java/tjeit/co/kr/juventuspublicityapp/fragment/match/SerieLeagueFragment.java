1df2c12f463b9d0c8a74e2f34dff7051d61439fb
henylee <asqaws1@gmail.com>
2017-10-23 18:18:33 +0900

    경기일정 구현 완료!!!

--- app/src/main/java/tjeit/co/kr/juventuspublicityapp/fragment/match/SerieLeagueFragment.java
+++ app/src/main/java/tjeit/co/kr/juventuspublicityapp/fragment/match/SerieLeagueFragment.java
@@ -72,7 +72,7 @@ public class SerieLeagueFragment extends Fragment {
         this.calendarView = (HorizontalCalendarView) v.findViewById(R.id.calendarView);
 
         Calendar endDate = Calendar.getInstance();
-        endDate.add(Calendar.MONTH, 7);
+        endDate.add(Calendar.MONTH, 8);
         Calendar startDate = Calendar.getInstance();
         startDate.add(Calendar.MONTH, -2);
         horizontalCalendar = new HorizontalCalendar.Builder(v, R.id.calendarView).startDate(startDate.getTime()).endDate(endDate.getTime()).datesNumberOnScreen(5).dayNameFormat("EEE").dayNumberFormat("dd").monthFormat("MMM").textSize(14f, 24f, 14f).showDayName(true).showMonthName(true).textColor(Color.LTGRAY, Color.WHITE).selectedDateBackground(Color.TRANSPARENT).build();
@@ -104,7 +104,6 @@ public class SerieLeagueFragment extends Fragment {
                 }
 
                 if (showMatch != null) {
-
                     matchLayout.setVisibility(View.VISIBLE);
                     notMatchLayout.setVisibility(View.GONE);
                     homeTeamNameTxt.setText(showMatch.getHomeTeamName());
