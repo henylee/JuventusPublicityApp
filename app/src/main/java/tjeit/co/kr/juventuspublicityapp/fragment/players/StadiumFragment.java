c00d51375f9a9155e08a9f83d4b4c9116d07942f
henylee <asqaws1@gmail.com>
2017-10-24 10:34:48 +0900

    구글맵 오류!

--- app/src/main/java/tjeit/co/kr/juventuspublicityapp/fragment/players/StadiumFragment.java
+++ app/src/main/java/tjeit/co/kr/juventuspublicityapp/fragment/players/StadiumFragment.java
@@ -7,18 +7,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import tjeit.co.kr.juventuspublicityapp.R;
 
 /**
  * Created by joeun on 2017-10-19.
  */
 
-public class StadiumFragment extends Fragment{
+public class StadiumFragment extends Fragment {
+
+    private GoogleMap mMap;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frag_stadium, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+
+                LatLng stadium = new LatLng(50, 10151);
+                mMap.addMarker(new MarkerOptions().position(stadium).title("Allianz Stadium"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(stadium));
+            }
+        });
         return v;
     }
 
